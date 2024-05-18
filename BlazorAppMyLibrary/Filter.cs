884b427e68f0bb44c7abd037e258e27c11b48974
Guy Kroyturo <guysystem@gmail.com>
2024-05-18 15:26:22 +0300

    new

--- BlazorAppMyLibrary/Filter.cs
+++ BlazorAppMyLibrary/Filter.cs
@@ -9,107 +9,37 @@ namespace BlazorAppMyLibrary
 {
     public class Filter
     {
-        private string bookNameTxt;
+       public string BookNameTxt { get; set; }
+       public int GenreBookCode { get; set; }
+       public int WriterBookCode { get; set; }
 
-        private int genreBookCode;
+       public string BuildFilter()
+       {
+            string query = "SELECT * FROM bookTbl WHERE";
 
-        private int writerBookCode;
-
-        public string BookNameTxt { get => bookNameTxt; set => bookNameTxt = value; }
-        public int GenreBookCode { get => genreBookCode; set => genreBookCode = value; }
-        public int WriterBookCode { get => writerBookCode; set => writerBookCode = value; }
-
-
-        public string BuildFilter()
-        {
-
-            int codeSwich = 0;
-            if (bookNameTxt != null && genreBookCode != 0 && writerBookCode != 0)
+            if (!string.IsNullOrEmpty(BookNameTxt))
             {
-                codeSwich = 1;
-            }
-            else
-            {
-                if (bookNameTxt != null && genreBookCode != 0 && writerBookCode == 0)
-                {
-                    codeSwich = 2;
-                }
-                else
-                {
-                    if (genreBookCode != 0 && writerBookCode != 0 && bookNameTxt == null)
-                    {
-                        codeSwich = 3;
-                    }
-                    else
-                    {
-                        if (writerBookCode != 0 && bookNameTxt != null && genreBookCode == 0)
-                        {
-                            codeSwich = 4;
-                        }
-                        else
-                        {
-                            if (bookNameTxt != null && genreBookCode == 0 && writerBookCode == 0)
-                            {
-                                codeSwich = 5;
-                            }
-                            else
-                            {
-                                if (bookNameTxt == null && genreBookCode != 0 && writerBookCode == 0)
-                                {
-                                    codeSwich = 6;
-                                }
-                                else
-                                {
-                                    if (bookNameTxt == null && genreBookCode == 0 && writerBookCode != 0)
-                                    {
-                                        codeSwich = 7;
-                                    }
-                                    else
-                                    {
-                                        codeSwich = 8;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+                query += $" bookName LIKE '{BookNameTxt}%' AND";
             }
 
-
-            // Using Swich Case
-
-            switch (codeSwich)
+            if(GenreBookCode != 0)
             {
-                case 1:
-                    return $"select * from bookTbl where bookName like '{BookNameTxt}%' and genreCode = {GenreBookCode} and writerCode = {WriterBookCode} ";
-                    break;
-                case 2:
-                    return $"select * from bookTbl where bookName like '{BookNameTxt}%' and genreCode = {GenreBookCode}";
-                    break;
-                case 3:
-                    return $"select * from bookTbl where genreCode = {GenreBookCode} and writerCode = {WriterBookCode} ";
-                    break;
-                case 4:
-                    return $"select * from bookTbl where bookName like '{BookNameTxt}%' and writerCode = {WriterBookCode}";
-                    break;
-                case 5:
-                    return $"select * from bookTbl where bookName like '{BookNameTxt}%' ";
-                    break;
-                case 6:
-                    return $"select * from bookTbl where genreCode = {GenreBookCode} ";
-                    break;
-                case 7:
-                    return $"select * from bookTbl where writerCode = {WriterBookCode} ";
-                    break;
-                case 8:
-                    return $"select * from bookTbl";
-                    break;
-                default:
-                    return $"select * from bookTbl";
-                    break;
+                query += $" genreCode = {GenreBookCode} AND";
             }
 
-        }
+            if(WriterBookCode != 0)
+            {
+                query += $" writerCode = {WriterBookCode} ";
+            }
+
+            if(query.EndsWith(" AND") || query.EndsWith(" WHERE"))
+            {
+                query = query.Remove(query.Length - 4);
+            }
+
+            return query;
+       }
+
 
     }
 }
